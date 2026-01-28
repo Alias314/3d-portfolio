71871c33312d9ad16560d0c42fcf272515f098e7
Alias314 <83777973+Alias314@users.noreply.github.com>
2026-01-28 11:50:24 +0800

    Added Canvas and Scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Canvas } from "@react-three/fiber";
+import { useEffect, useState } from "react";
+import Scene from "./scene/Scene";
 
 function App() {
   return (
-    <div className="w-screen h-screen">
-      
+    <div className="relative w-screen h-screen">      
+      <Canvas camera={{ position: [3, 5, 3] }} shadows>
+        <Scene />
+      </Canvas>
     </div>
   );
 };
